cb6f71503d0d733410f02474ab59db5bbdb72fee
Miguel I5 <Miguel I5@192.168.1.37>
2017-06-12 16:54:12 +0200

    Version 2

--- Ejercicios/src/main/java/Java/Ejercicios/Class_Main.java
+++ Ejercicios/src/main/java/Java/Ejercicios/Class_Main.java
@@ -10,12 +10,25 @@ public class Class_Main {
 	
 	public static void main( String[] args ){
 		inicio_programa();
+		System.out.println("Ejercicio 1:");
 		Ejercicio1_CadenaCaracter.metodo('c', "miguel");
+		System.out.println(" ");
+		System.out.println("Ejercicio 2:");
 		Ejercicio2_CadenaCaracterCont.metodo('a', "atleticodemadrid");
+		System.out.println(" ");
+		System.out.println("Ejercicio 3:");
 		Ejercicio3_NumParImpar.metodo(5);
+		System.out.println(" ");
+		System.out.println("Ejercicio 4:");
 		Ejercicio4_MayorEdad.metodo(18);
+		System.out.println(" ");
+		System.out.println("Ejercicio 5:");
 		Ejercicio5_Notas.metodo(5);
+		System.out.println(" ");
+		System.out.println("Ejercicio 6:");
 		Ejercicio6_CadenaAlReves.metodo("holaquetalestasyobien");
+		System.out.println(" ");
+		System.out.println("Ejercicio 7:");
 		Ejercicio7_Secuencia.metodo();
 	}
 }
